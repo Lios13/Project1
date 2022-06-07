3564b96fca67251796645104636f749c221a1b0e
Lios13 <106203498+Lios13@users.noreply.github.com>
2022-06-07 14:56:51 -0500

    Add files via upload

---
+++ plantilla-del-proyecto-C9-main/sketch.js
@@ -0,0 +1,41 @@
+
+function setup() {
+  createCanvas(400,400);
+  background(51);
+  box = createSprite(200,200,30,30);
+
+}
+
+function draw() 
+{
+
+  // escribir el código para cambiar el color del fondo 
+  // rojo cuando se presiona RIGHT_ARROW (tecla de flecha derecha)
+  
+  
+
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("blue");
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("yellow");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
+  }
+  if (keyIsDown(RIGHT_ARROW)) 
+  {
+    background("red");
+  }
+
+  
+  drawSprites();
+}
+

5dd6c110e0844b52a49718e784de08c6c7fe5b48
Lios13 <106203498+Lios13@users.noreply.github.com>
2022-06-15 22:01:39 -0500

    Add files via upload

---
+++ Plantilla-del-proyecto-C11-main/sketch.js
@@ -0,0 +1,57 @@
+var path,boy, leftBoundary,rightBoundary;
+var pathImg,boyImg;
+var i;
+
+function preload(){
+  pathImg = loadImage("path.png");
+  boyImg = loadAnimation("Runner-1.png","Runner-2.png");
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+  
+// Mover el fondo
+path=createSprite(200,200);
+path.addImage(pathImg);
+path.velocityY = 4;
+path.scale=1.2;
+
+//crear sprite de niño corriendo
+boy = createSprite(180,340,30,30);
+boy.scale=0.08;
+boy.addAnimation("JakeRunning",boyImg);
+  
+
+leftBoundary=createSprite(0,0,100,800);
+
+// leftBoundary.invisible = false;
+// leftBoundary.visible = true;
+ //leftBoundary.invisible = true;
+ leftBoundary.visible = false;
+
+
+rightBoundary=createSprite(410,0,100,800);
+rightBoundary.visible = false;
+}
+
+function draw() {
+  background(0);
+  path.velocityY = 4;
+  
+  boy.x = World.mouseX;
+  
+  edges= createEdgeSprites();
+  boy.collide(edges[3]);
+  boy.collide(leftBoundary);
+  boy.collide(rightBoundary);
+  
+  //código para reiniciar el fondo
+
+  if(path.y > 400 ){
+    path.y = height/2;
+  }
+
+  
+  drawSprites();
+}

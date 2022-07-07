93386c764175f1042b366abc6f35618b334994b6
Lios13 <106203498+Lios13@users.noreply.github.com>
2022-07-06 21:12:13 -0500

    Add files via upload

---
+++ Plantilla-del-proyecto-C12-main/sketch.js
@@ -0,0 +1,114 @@
+var garden,rabbit,apple,orangeL,redL;
+var gardenImg,rabbitImg,carrotImg,orangeImg,redImg;
+
+
+function preload(){
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+  appleImg = loadImage("apple.png");
+  orangeImg = loadImage("orangeLeaf.png");
+  redImg = loadImage("redImage.png");
+}
+
+
+function setup(){
+  
+  createCanvas(400,400);
+// Mover el fondo
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
+
+
+//crear sprite del conejo moviéndose
+rabbit = createSprite(160,340,20,20);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
+}
+
+function draw() {
+  background(0);
+  
+  // mover al conejo en el eje X con el mouse
+  rabbit.x = World.mouseX;
+  
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+  
+   drawSprites();
+   
+  
+//var select_sprites = Math(random(1,3));
+
+ var appleImg = Math.random(random(1,3));
+
+// var select_sprites = Math.round(1,3);
+
+// var select_sprites = Math.round(random(1,3));
+
+  
+   if (frameCount % 50 == 0) {
+     if (appleImg == 1) {
+      createApples();
+     } else if (orangeImg == 2) {
+       createOrange();
+     }else {
+     createRed();
+    }
+  }
+
+  // if (frameCount % 80 == 0) {
+  //   if (select_sprites == 1) {
+  //     createApples();
+  //   } else if (select_sprites == 2) {
+  //     createOrange();
+  //   }
+  // }
+
+  // if (frameCount / 80 == 0) {
+  //   if (select_sprites == 1) {
+  //     createApples();
+  //   } else if (select_sprites == 2) {
+  //     createOrange();
+  //   }else {
+  //     createRed();
+  //   }
+  // }
+
+  // if (frameCount % 80 = 0) {
+  //   if (select_sprites == 1) {
+  //     createApples();
+  //   } else if (select_sprites == 2) {
+  //     createOrange();
+  //   }else {
+  //     createRed();
+  //   }
+  // }
+
+
+
+}
+
+function createApples() {
+apple = createSprite(random(50, 350),40, 10, 10);
+apple.addImage(appleImg);
+apple.scale=0.07;
+apple.velocityY = 3;
+apple.lifetime = 150;
+  
+}
+
+function createOrange() {
+orangeL = createSprite(random(50, 350),40, 10, 10);
+orangeL.addImage(orangeImg);
+orangeL.scale=0.08;
+orangeL.velocityY = 3;
+orangeL.lifetime = 150;
+}
+
+function createRed() {
+redL = createSprite(random(50, 350),40, 10, 10);
+redL.addImage(redImg);
+redL.scale=0.06;
+  redL.velocityY = 3;
+  redL.lifetime = 150;
+}
